5014f74b9f55516fd69fe7ef12478b1cf87d2e8a
Szyu <julianheinen@gmx.de>
2016-11-02 11:13:44 +0100

    added farseer reference

--- StarrockGame/Game1.cs
+++ StarrockGame/Game1.cs
@@ -39,7 +39,7 @@ namespace StarrockGame
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            
             // TODO: use this.Content to load your game content here
         }
 
